93dc0f0329bd7be48607990ef1b4fbe13b545501
przemyslaw zaremba <rpezemk@gmail.com>
2023-11-21 23:20:25 +0100

    SID, dont touch

---
+++ ZImageTests/GUI/Controls/ZImage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using SixLabors.ImageSharp; 
+using SixLabors.ImageSharp.PixelFormats;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PZControlsWpf.Converters;
+using PZWrapper.Types;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ZImageTests.Controls
+{
+    /// <summary>
+    /// Interaction logic for ZImage.xaml
+    /// </summary>
+    public partial class ZImage : UserControl
+    {
+        public ZImage()
+        {
+            InitializeComponent();
+        }
+
+        public void Show(Matrix2D image, Stretch stretch) { ZImageDisp.Source = AutoConverter.ConvChain.Convert<Matrix2D, ImageSource>(image); ZImageDisp.Stretch = stretch; }
+        public void Show(ImageSource imageSource, Stretch stretch) { ZImageDisp.Source = imageSource; ZImageDisp.Stretch = stretch; }
+
+    }
+}
