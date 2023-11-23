4e418d31d081fc7d8680eade5c1b6182448017e6
przemyslaw zaremba <rpezemk@gmail.com>
2023-11-23 22:24:23 +0100

    Nothing....

---
+++ ZImageTests/VM/ImageFame_VM.cs
@@ -0,0 +1,15 @@
+﻿using PZWrapper.Types;
+using ZImageTests.VM.Basic;
+
+namespace ZImageTests.VM
+{
+    public class ImageFame_VM : ObservableObject
+    {
+        public AMatrix Matrix;
+
+    }
+
+
+
+
+}
