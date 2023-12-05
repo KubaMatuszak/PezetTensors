2a73bbba76da6b56a9f727e8d001d26dc6efb4f9
przemyslaw zaremba <rpezemk@gmail.com>
2023-12-05 12:36:19 +0100

    VisualNode added. As far -- testing purposes.

--- ProcessTest/MainWindow.xaml.cs
+++ ProcessTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProcessTest.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,62 @@ namespace ProcessTest
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.Children.Clear();
+
+            var root = NodeTests.GimmeTree();
+
+            var list = root.AsList();
+
+
+            foreach(var node in list)
+            {
+                var visNode = new VisualNode();
+                MyCanvas.Children.Add(visNode);
+                Canvas.SetLeft(visNode, node.X);
+                Canvas.SetTop(visNode, node.Y);
+
+
+                foreach(var ch in node.Children)
+                {
+                    var geom = CreateSplineBetweenPoints(
+                        new Point(node.X+100, node.Y+25),
+                        new Point(ch.X, ch.Y+25));
+                    Path path = new Path();
+                    path.Stroke = Brushes.RosyBrown;
+                    path.StrokeThickness = 3;
+                    path.Data = geom;
+                    MyCanvas.Children.Add(path);
+
+                    geom = CreateSplineBetweenPoints(
+                        new Point(node.X + 100, node.Y + 25),
+                        new Point(ch.X, ch.Y + 25));
+                    Path path2 = new Path();
+                    path2.Stroke = Brushes.White;
+                    path2.StrokeThickness = 1;
+                    path2.Data = geom;
+                    MyCanvas.Children.Add(path2);
+                }
+
+            }
+        }
+
+        public static PathGeometry CreateSplineBetweenPoints(Point startPoint, Point endPoint)
+        {
+            PathFigure pathFigure = new PathFigure() { StartPoint = startPoint };
+            var dx = endPoint.X - startPoint.X;
+            var dxAbs = Math.Abs(dx);
+            var dy = endPoint.Y - startPoint.Y;
+            var beg = dx/3;
+            var rc = new Point(startPoint.X + beg, startPoint.Y);
+            var lc = new Point(endPoint.X - beg, endPoint.Y);
+            pathFigure.Segments.Add(new BezierSegment(rc, lc, endPoint, true));
+            PathGeometry pathGeometry = new PathGeometry();
+            pathGeometry.Figures.Add(pathFigure);
+            return pathGeometry;
         }
     }
 }
---
+++ ProcessTest/NodeTests.cs
@@ -0,0 +1,26 @@
+﻿using ProcessTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTest
+{
+    public static class NodeTests
+    {
+        public static Node GimmeTree()
+        {
+            Node root = new Node() { Name = "Root node 00", X = 50, Y = 40 };
+            Node node1 = new Node() { Name = "node 01", X = 130, Y = 130 };
+            Node node2 = new Node() { Name = "node 02", X = 320, Y = 160 };
+            Node node3 = new Node() { Name = "node 03", X = 550, Y = 350 };
+            root.AppendNext(node1).AppendNext(node3);
+            root.AppendNext(node2).AppendNext(node3);
+            return root;
+        }
+
+        
+        
+    }
+}
