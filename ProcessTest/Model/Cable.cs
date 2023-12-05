a397b9fcd037f1f6b56655271e4b37fd3059b310
przemyslaw zaremba <rpezemk@gmail.com>
2023-12-05 20:35:16 +0100

    Added Cable and Workflow models.

---
+++ ProcessTest/Model/Cable.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessTest.Model
+{
+    public class Cable
+    {
+        public Cable() { }
+        public Node FromNode { get; set; }
+        public Node ToNode { get; set; }
+    }
+}
---
+++ ProcessTest/Model/Workflow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTest.Model
+{
+    class Workflow
+    {
+        public List<Node> Nodes { get; set; }
+        public List<Cable> Cables { get; set; }
+    }
+}
