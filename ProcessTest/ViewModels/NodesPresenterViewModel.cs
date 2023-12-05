8a092ff453defd00ef222607279e43aa3b39bc2a
przemyslaw zaremba <rpezemk@gmail.com>
2023-12-05 13:00:59 +0100

    Added NodesPresenterViewModel

---
+++ ProcessTest/ViewModels/NodesPresenterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ProcessTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTest.ViewModels
+{
+    public class NodesPresenterViewModel : ObservableObject
+    {
+		private ObservableCollection<Node> nodes;
+
+		public ObservableCollection<Node> Nodes
+		{
+			get { return nodes; }
+			set => SetProperty(ref nodes, value);
+		}
+	}
+}
